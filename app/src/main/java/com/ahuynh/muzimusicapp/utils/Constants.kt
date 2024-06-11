9903f679a8bb86af7e3953e7392783f4e3775f96
ahuynh359 <ahuynh359@gmail.com>
2024-06-11 20:55:59 +0700

    update

--- app/src/main/java/com/ahuynh/muzimusicapp/utils/Constants.kt
+++ app/src/main/java/com/ahuynh/muzimusicapp/utils/Constants.kt
@@ -16,7 +16,7 @@ object Constants {
     var INTENT_ACTION = "intent_service"
 
     var USER_ID: Long = 0
-    const val BASE_URL = "http://192.168.138.210:8765"
+    const val BASE_URL = "http://192.168.138.65:8765"
     const val API_VERSION = ""
 
 
